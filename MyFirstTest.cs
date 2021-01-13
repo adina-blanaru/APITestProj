b199579a75b35eb84bd8771c2a94fe0789c8d38e
Adina Blanaru <>
2021-01-13 20:39:55 +0200

    Add project files.

---
+++ MyFirstTest.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+
+namespace APITestProj
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        [Test]
+        public void ValidateGetBookingRequest()
+        {
+            string myBookingId = "10";
+            //create client connection
+            //RestClient restClient = new RestClient("https://restful-booker.herokuapp.com/booking/10");
+            RestClient restClient = new RestClient($"https://restful-booker.herokuapp.com/booking/{myBookingId}");
+
+            //create request get data
+            RestRequest restRequest = new RestRequest(Method.GET);
+
+            //execute request to server
+            IRestResponse restResponse = restClient.Execute(restRequest.AddHeader("Accept", "application/json"));
+
+            //extract data from response
+            var response = restResponse.Content;
+
+            //assert            
+            Assert.IsTrue(response.Contains("Sally"));
+        }
+
+        [Test]
+        public void ValidatePostRequest()
+        {
+            //create client connection
+            RestClient restClient = new RestClient("https://restful-booker.herokuapp.com/auth");
+            JObject objectBody = new JObject();
+            objectBody.Add("username", "admin");
+            objectBody.Add("password", "password123");
+
+            //create request post data
+            RestRequest restRequest = new RestRequest(Method.POST);
+            restRequest.AddParameter("application/json", objectBody, ParameterType.RequestBody);
+
+            //execute request to server
+            IRestResponse response = restClient.Execute(restRequest);
+
+            //assert            
+            Assert.IsTrue(response.Content.Contains("token"));
+            Assert.AreEqual(response.StatusCode.ToString(),"OK");
+            Assert.AreEqual((int)response.StatusCode,200);
+
+            //
+            //restRequest.RequestFormat = DataFormat.Json;
+            //restRequest.AddBody();
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITestProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITestProj")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30b0939b-1a68-4928-95e7-d6efb1c8c70d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
